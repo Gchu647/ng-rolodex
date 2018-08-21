ed521880f1eeb6c3e7ac00fa6845a91bf74c4203
gchu <gchu647@gmail.com>
2018-08-20 21:43:42 -1000

    got PUT user route to work with a temp id

--- server/routes/systems.js
+++ server/routes/systems.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const User = require('../db/models/User');
 
 router.get('/profile', (req, res) => { // Fetches the profile of a user
-  console.log(req.query.user);
   const id = req.query.user;
 
   return new User()
@@ -14,5 +13,28 @@ router.get('/profile', (req, res) => { // Fetches the profile of a user
     })
     .catch(err => res.json(err.message));
  });
+
+ router.put('/users/:id' , (req, res) => { //take out the id later
+  const id = req.params.id;
+  const username = req.body.username ? req.body.username.trim() : null;
+  const name = req.body.name.trim();
+  const email = req.body.email.trim();
+  const address = req.body.address.trim();
+
+  const editInfo = {
+    username, 
+    name,
+    email,
+    address,
+  }
+
+  return new User()
+    .where({ id })
+    .save(editInfo, {'patch': true})
+    .then(profile => {
+      res.json(profile);
+    })
+    .catch(err => res.json(err.message));
+ })
 
 module.exports = router;
