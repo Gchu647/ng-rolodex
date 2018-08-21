54daa789b7e2d902cba303dc3cd84ddd43c25124
gchu <gchu647@gmail.com>
2018-08-20 16:02:22 -1000

    housekeeping

---
+++ src/app/rolodex/rolodex.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rolodex',
+  templateUrl: './rolodex.component.html',
+  styleUrls: ['./rolodex.component.scss']
+})
+
+export class RolodexComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+}
