33963eb1bec151f1b744c13ee8191fc8e976f083
gchu <gchu647@gmail.com>
2018-08-22 12:28:05 -1000

    smoke test from frontend to get contacts in the backend works

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -14,9 +14,9 @@ export class BackendService {
 
   // WORKING on
   contacts() {
-    // const contactUrl = this.url + 'contact';
-    // console.log('contact backend');
-    // return this.http.post(contactUrl, null).toPromise();
-    return Promise.resolve({});
+    const contactUrl = this.url + 'contacts?user=1'; // hard coded
+    console.log('contacts backend services');
+    return this.http.get(contactUrl).toPromise();
+    // return Promise.resolve({});
   }
 }
