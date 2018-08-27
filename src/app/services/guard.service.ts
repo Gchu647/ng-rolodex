7822bd61b61a00d04b2e2c14b6cb093a894ef0c0
gchu <gchu647@gmail.com>
2018-08-27 11:13:56 -1000

    added redirect to guard service

--- src/app/services/guard.service.ts
+++ src/app/services/guard.service.ts
@@ -1,4 +1,4 @@
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { SessionService } from './session.service';
 
@@ -6,9 +6,17 @@ import { SessionService } from './session.service';
   providedIn: 'root'
 })
 export class GuardService implements CanActivate {
-  constructor( private session: SessionService) {}
+  constructor( 
+    private session: SessionService,
+    private router: Router,
+  ) {}
 
   canActivate() {
-    return this.session.isLoggedIn();
+    if(!this.session.isLoggedIn()) {
+      this.router.navigate(['/']);
+      return false;
+    } else {
+      return true;
+    }
   }
 }
